2d7f2f295560ea3fa880c528469d957e27d64f6a
Matthew DeVerna <mdeverna@iu.edu>
2023-10-24 21:33:38 -0400

    refactor functions into package

--- scripts/scrape_politifact_all.py
+++ scripts/scrape_politifact_all.py
@@ -19,15 +19,13 @@ Author: Matthew DeVerna
 import json
 import os
 import random
-import requests
 import time
 
 import pandas as pd
 
-from bs4 import BeautifulSoup
-from requests.exceptions import RequestException
-
 from politifact_pkg import PolitiFactCheck
+from politifact_pkg.utils import fetch_url, find_max_page
+from politifact_pkg.parsing import extract_statement_links
 
 #### IMPORTANT!!! ####
 # Set to some number that is higher than the number of pages of fact checks
@@ -42,157 +40,6 @@ DATA_DIR = "../data"
 FC_CACHE = os.path.join(DATA_DIR, "factchecks_cache.json")
 FC_PARQUET = os.path.join(DATA_DIR, "factchecks.parquet")
 MISSED_LINKS = os.path.join(DATA_DIR, "missed_factcheck_links.txt")
-
-
-def fetch_url(url, max_retries=7, retry_delay=2):
-    """
-    Fetches data for a provided web URL.
-
-    Parameters
-    ----------
-    - url (str): The URL of the webpage.
-    - max_retries (int): Maximum number of retries in case of failure.
-    - retry_delay (int): Number of seconds to wait between retries.
-
-    Returns
-    ----------
-    - str: The HTML content of the webpage, or None in case of failure.
-
-    Exceptions
-    ----------
-    - TypeError
-    """
-    if not isinstance(url, str):
-        raise TypeError("`url` must be a string")
-    if not isinstance(max_retries, int):
-        raise TypeError("`max_retries` must be an integer")
-    if not isinstance(retry_delay, int):
-        raise TypeError("`retry_delay` must be an integer")
-
-    for attempt in range(max_retries):
-        try:
-            response = requests.get(url)
-            response.raise_for_status()  # Raise an exception for HTTP errors
-            return response
-
-        except RequestException as e:
-            if isinstance(e, requests.ConnectionError):
-                # Handle network-related errors
-                print(f"Attempt {attempt + 1} failed due to a network error: {e}")
-            elif isinstance(e, requests.Timeout):
-                # Handle timeout errors
-                print(f"Attempt {attempt + 1} timed out: {e}")
-            else:
-                # Handle other RequestException errors
-                print(f"Attempt {attempt + 1} failed with an unknown error: {e}")
-
-            if attempt < max_retries - 1:
-                print(f"Retrying in {retry_delay} seconds...")
-                time.sleep(retry_delay * attempt)
-            else:
-                print("Max retries reached. Failed to fetch the webpage.")
-                return None
-
-
-def find_no_results(soup):
-    """
-    Check if a politifact page has no results.
-
-    Parameters:
-    ----------
-    - soup (BeautifulSoup): HTML content of the webpage.
-
-    Returns:
-    ----------
-    - bool: True if the page has no results, False otherwise.
-    """
-    # Find the <h2> element with the specified class.
-    h2_element = soup.find("h2", class_="c-title c-title--subline")
-
-    # Check if the element exists and contains the expected text.
-    if h2_element and "No Results found" in h2_element.get_text():
-        return True
-    else:
-        return False
-
-
-def find_max_page(base_url, max_page):
-    """
-    Find the maximum page number for the politifact base URL.
-
-    Parameters:
-    ----------
-    - base_url (str): The base URL used to construct the politifact URLs.
-    - max_page (int): The maximum page number to search.
-
-    Returns:
-    ----------
-    - int: The maximum page number with politifact results.
-
-    """
-    no_results = True
-
-    while no_results and max_page > 0:
-        # Decrement max_page by 1
-        max_page -= 1
-
-        # Sleep for a bit to be nice to politifact
-        time.sleep(0.3 + random.random())
-
-        # Construct the URL
-        url = f"{base_url}{max_page}"
-
-        # Fetch the page
-        print(f"Trying: {url}")
-        response = fetch_url(url)
-        soup = BeautifulSoup(response.text, "html.parser")
-
-        # Once this returns False, we have found results, and should take
-        # this as our maximum page.
-        no_results = find_no_results(soup)
-
-    return max_page
-
-
-def extract_statement_links(response):
-    """
-    Extracts links from requests.models.Response object from politifact.
-
-    Parameters:
-    ----------
-    - response (requests.models.Response): HTML content of the webpage.
-
-    Returns:
-    ----------
-    - statement_links (List(str)): A list of extracted href attributes.
-
-    Exceptions
-    ----------
-    - TypeError
-    """
-    if not isinstance(response, requests.models.Response):
-        raise TypeError(f"`response` must be of type `requests.models.Response`")
-    if not response.url.startswith(FC_LIST_URL):
-        raise TypeError(
-            f"`response` URL must start with {FC_LIST_URL}. "
-            f"Currently URL is: {response.url}"
-        )
-
-    statement_links = []
-    if response:
-        html_text = response.text
-        soup = BeautifulSoup(html_text, "html.parser")
-
-        # Find all elements with the "m_statement__quote" class
-        statement_divs = soup.find_all("div", attrs={"class": "m-statement__quote"})
-
-        for element in statement_divs:
-            # Extract the href attribute if it exists
-            href = element.find("a").get("href")
-            if href:
-                statement_links.append(href)
-
-    return statement_links
 
 
 if __name__ == "__main__":
@@ -230,7 +77,7 @@ if __name__ == "__main__":
                 break
 
             print(f"\t- Parsing fact checks...")
-            statement_links = extract_statement_links(response)
+            statement_links = extract_statement_links(response, FC_LIST_URL)
             print(f"\t- Found {len(statement_links)} links.")
 
             for idx, link in enumerate(statement_links, start=1):
@@ -249,7 +96,7 @@ if __name__ == "__main__":
                     print(f"\t- Failed. Saving to missed file.")
                     print(e)
                     with open(MISSED_LINKS, "a") as mf:
-                        mf.write(f"{link}\n")
+                        mf.write(f"{full_url}\n")
                     continue
 
                 print(f"\t\t- Success.")
